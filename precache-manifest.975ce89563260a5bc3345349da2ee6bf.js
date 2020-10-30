4412797ae6d812734017c5f7cd8dc3789680f3ab
glenn <glenn.ludszuweit@gmail.com>
2020-10-30 10:01:46 +0100

    Updates

---
+++ precache-manifest.975ce89563260a5bc3345349da2ee6bf.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc0068e3e1e2f25ea6fa68d21a99d90",
+    "url": "/wikiApp-React/index.html"
+  },
+  {
+    "revision": "a0ec1bbe251c5eebeb81",
+    "url": "/wikiApp-React/static/css/2.e8f1b9b5.chunk.css"
+  },
+  {
+    "revision": "140b4359f1f6e6fe387e",
+    "url": "/wikiApp-React/static/css/main.bbfaffa1.chunk.css"
+  },
+  {
+    "revision": "a0ec1bbe251c5eebeb81",
+    "url": "/wikiApp-React/static/js/2.2c9b2892.chunk.js"
+  },
+  {
+    "revision": "78d5c116b79864ac75ec03483319b43a",
+    "url": "/wikiApp-React/static/js/2.2c9b2892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140b4359f1f6e6fe387e",
+    "url": "/wikiApp-React/static/js/main.4d2a47b8.chunk.js"
+  },
+  {
+    "revision": "30e6e050956b08925cd9",
+    "url": "/wikiApp-React/static/js/runtime-main.99ef6d50.js"
+  },
+  {
+    "revision": "63bb13ca02136d6e8cb532ae9b355a26",
+    "url": "/wikiApp-React/static/media/ball.63bb13ca.png"
+  },
+  {
+    "revision": "e09f37c5ca554f42fc904cfaf998daea",
+    "url": "/wikiApp-React/static/media/home.e09f37c5.jpg"
+  },
+  {
+    "revision": "dfd7f090ac414e4824ec95165262abff",
+    "url": "/wikiApp-React/static/media/logo.dfd7f090.png"
+  },
+  {
+    "revision": "82fa597f29de41cd41a7c402bcf09ba5",
+    "url": "/wikiApp-React/static/media/summernote.82fa597f.ttf"
+  },
+  {
+    "revision": "c1a96d26d30d9e0b2fd33c080d88c72e",
+    "url": "/wikiApp-React/static/media/summernote.c1a96d26.woff"
+  },
+  {
+    "revision": "f4a47ce92c02ef70fc848508f4cec94a",
+    "url": "/wikiApp-React/static/media/summernote.f4a47ce9.eot"
+  },
+  {
+    "revision": "f694db69cded200e4edd999fddef81b7",
+    "url": "/wikiApp-React/static/media/summernote.f694db69.woff2"
+  }
+]);
